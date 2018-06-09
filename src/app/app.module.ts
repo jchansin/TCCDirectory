58d647073e71a599abb5916a5c1573ae5f25b469
Jerome <jchansin.tcc@gmail.com>
2018-06-09 13:27:14 -1000

    Création des pages Installation des plug-ins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Geolocation } from '@ionic-native/geolocation';
+import { CallNumber } from '@ionic-native/call-number';
+import { SMS } from '@ionic-native/sms';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SearchPage } from '../pages/search/search';
+import { MapPage } from '../pages/map/map';
+import { ListPage } from '../pages/list/list';
+import { FavoritesPage } from '../pages/favorites/favorites';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SearchPage,
+    MapPage,
+    ListPage,
+    FavoritesPage
   ],
   imports: [
     BrowserModule,
@@ -19,11 +33,16 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SearchPage,
+    MapPage,
+    ListPage,
+    FavoritesPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    SQLite,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })

1adbea6664f3b2e6172c8fa60ff677c7993d229a
Jerome <jchansin.tcc@gmail.com>
2018-06-12 11:40:06 -1000

    InitDB working Search page working

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,12 +1,10 @@
 import { DatabaseProvider } from './../../services/database.service';
 import { FavoritesPage } from './../favorites/favorites';
 import { ListPage } from './../list/list';
-// import { MapPage } from './../map/map';
+import { MapPage } from './../map/map';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Http } from '@angular/http';
-
-import { TccdApiGlobal } from './../../models/tccdapi-global.model';
 import { TccdApiService } from './../../services/tccdapi.service';
 
 
@@ -16,36 +14,47 @@ import { TccdApiService } from './../../services/tccdapi.service';
 })
 export class SearchPage {
 
-  skillsList: TccdApiGlobal;
   skills = [];
+  selected_value: string;
+  value: string;
 
   constructor(public database: DatabaseProvider, public navCtrl: NavController, public navParams: NavParams, private http: Http, private tccdApiService: TccdApiService) {
-    /* this.tccdApiService.getBusiness(12).then ((resp) => {
-      console.log(JSON.stringify(resp)); 
-    }) */
-    
-    this.database.selectFavorite();
 
   }
 
-
   ionViewDidLoad() {
+
     console.log('ionViewDidLoad SearchPage');
+    this.skills = [];
     this.tccdApiService.getSkillsList()
     .then (response => {
-        console.log(JSON.stringify(response.data[0].name));
-        //this.skills = response.data.length;
-        for (let i = 0; i < response.data.length; i++) {
-            this.skills.push(response.data[i].name);
-    }})
+      console.log('Données de getSkillsList récupérées', JSON.stringify(response.data[0].name));
+      for (let i = 0; i < response.data.length; i++) {
+          this.skills.push(response.data[i]);
+      }
+    })
+
   }
 
   goToListPage(){
-      this.navCtrl.setRoot(ListPage)
+      this.navCtrl.push(ListPage)
   }
+
   goToFavoritesPage() {
-    this.navCtrl.setRoot(FavoritesPage);
-}
+    this.navCtrl.push(FavoritesPage);
+  }
+
+  //Get Selected Value
+  showselected($event) {
+    this.selected_value = $event;
+    console.log(this.selected_value);
+  }
+
+  makeSearch() {
+    this.navCtrl.push(MapPage, {
+      value: this.selected_value.toString()})
+  }
+
 
 
 }

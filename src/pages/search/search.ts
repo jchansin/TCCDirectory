2b331c86e0cbda760be8a495971cd0eddec58246
Jerome <jchansin.tcc@gmail.com>
2018-06-12 11:53:33 -1000

    Fixed bug Transition to map working

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -14,6 +14,7 @@ import { TccdApiService } from './../../services/tccdapi.service';
 })
 export class SearchPage {
 
+  skillArray: Array<string>;
   skills = [];
   selected_value: string;
   value: string;
